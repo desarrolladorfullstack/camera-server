4cb11256598b4c5a0041f21e231db7cde2138816
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-12-21 12:49:04 -0500

    Remove Scripts files (rebase without branch:scripts)

--- consultas_spiaview.sql
+++
@@ -1,8 +0,0 @@
-SELECT f.*, 1024*count(r.record_id) size records
-FROM files f
-INNER JOIN file_records x ON f.file_id = x.file_key
-INNER JOIN records r ON r.record_id = x.record_key
-GROUP BY f.file_id
-ORDER BY f.file_id;
-
-SELECT last_value FROM files_file_id_seq;
