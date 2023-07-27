70a4c7a30d300715106cc7651d3a9175096c6a02
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2023-07-27 17:18:33 -0500

    backup logs part 0.1
    scripts creation - ready for develop

---
+++ backup_logs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# :: Please run only with bash ..
+# TASKS:
+# 1. read files from logs {folders: (bk/dualcam/ logs/)}
+# 2. loop per date {format: %Y%m%d} named directories creation {from: %Y%m%01} date
+# 3. loop move files to date {format} named directories
+# 4. loop zip compression {level: -9} and remove files
+# 5. {force: 1} remove date {format} named directories
