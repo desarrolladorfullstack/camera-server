cd32597c25d3688b7755b10b81aac5b5e64bd8f9
qcmarcel <qc.marcel@gmail.com>
2023-07-31 12:07:10 -0500

    backup scripts 1
    backup_logs
    backup_media
    ARGS and recursive directory calling

--- backup_logs.sh
+++ backup_logs.sh
@@ -6,3 +6,114 @@
 # 3. loop move files to date {format} named directories
 # 4. loop zip compression {level: -9} and remove files
 # 5. {force: 1} remove date {format} named directories
+ROOT="/home/marcel/"
+SCRIPT_FILE="${ROOT}scripts/backup_log.sh"
+LOCATIONS=("bk/dualcam/" "logs/")
+if [[ "$1" != "" ]]
+then
+    {
+        IFS=',' read -ra LOCATIONS <<< "$1"
+    } || {
+        echo "ERROR: invalid LOCATIONS format => $1"
+    }
+fi
+FROM=$(date +"%Y-%m-01")
+if [[ "$2" != "" ]]
+then
+    {
+        FROM=$(date +"%Y-%m-%d" -d "$2")
+    } || {
+        echo "ERROR: invalid FROM format => $2"
+    }
+fi
+FORCE=1
+if [[ "$3" != "" ]]
+then
+    {
+        FORCE=$(( $3 ))
+    } || {
+        echo "ERROR: invalid FORCE format => $3"
+    }
+fi
+LEVEL="-9"
+if [[ "$4" != "" ]]
+then
+    {
+        LEVEL="$4"
+    } || {
+        echo "ERROR: invalid LEVEL format => $4"
+    }
+fi
+TO=$(date +"%Y-%m-%d")
+{
+    for location in ${LOCATIONS[*]}
+    do
+        {
+            location_dir=$(echo $location | sed "s/\~//g")
+            if [[ $location == "$ROOT"* ]]
+            then
+                cd $location_dir
+            else        
+                cd $ROOT$location_dir
+            fi
+            if [[ $location == *"/~" ]]
+            then                
+                directories=($(find . -maxdepth 1 -mindepth 1 -type d | sed -e "s/.\///g" ))
+                for inner_directory in ${directories[*]}
+                do 
+                    bash $SCRIPT_FILE "$ROOT$location$inner_directory/" "$FROM" "$FORCE" "$LEVEL"
+                done
+                continue
+            fi
+            date_from=$FROM
+            while [[ $date_from != $TO ]]
+            do
+                echo "date_from: $date_from"
+                {
+                    directory=$(echo $date_from | sed "s/-//g")
+                    {
+                        mkdir "$directory"; 
+                    } || {
+                        echo "Could not create directory $directory"
+                    }
+                    {
+                        mv *${date_from}*".error" "${directory}/";
+                        mv *${date_from}*".errors" "${directory}/";
+                    } || { 
+                        echo "could not move error files to $directory"
+                    }
+                    {
+                        mv *${date_from}*".log" "${directory}/"; 
+                        mv *${directory}*".log" "${directory}/"; 
+                    } || { 
+                        echo "could not move log files to $directory"
+                    }
+                    {
+                        zip $LEVEL -r "${directory}.zip" "${directory}/"
+                    } || { 
+                        echo "could not compress files on $directory"
+                    }
+                    if (( 1 <= $FORCE ))
+                    then
+                        rm -Rf "${directory}/"*
+                    fi
+                } || { 
+                    echo "ERROR: Failed to backup ${directory}"
+                }
+                {
+                    date_from=$(date +"%s" -d "$date_from") 
+                    date_from=$(( $date_from + 86400 ))
+                    date_from=$(date +"%Y-%m-%d" -d "@$date_from") 
+                    echo "date_from changed: $date_from"
+                } || {
+                    echo "ERROR: Failed to update date_from: $date_from"
+                    break
+                }
+            done
+        } || {
+            echo "ERROR: location not found: $location"
+        }
+    done
+} || {
+    echo "ERROR: could not execute backup commands on :"${LOCATIONS[*]}
+}
